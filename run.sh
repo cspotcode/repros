657829e34e41e395bf99872285f30e04258fe4fa
Andrew Bradley <cspotcode@gmail.com>
2021-05-24 15:57:46 -0400

    add repro

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+rm -rf plop || true
+git clone --branch webpack https://github.com/plopjs/plop
+
+cd plop
+
+npm install
+npm run build
+npm pack
+npm install -g plop*.tgz
+
+cd ..
+
+which plop
+plop --help
+plop
